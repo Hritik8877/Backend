36da97f42ba1cb0f0e317333840612c1ea898abe
Hritik kumar <hritikkyd8877@gmail.com>
2025-01-25 01:18:04 +0530

    all setup  done

--- src/app.js
+++ src/app.js
@@ -16,6 +16,13 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded())
 app.use(express.static("public"))
+app.use(cookieParser())
 
+//routes
+import userRoutes from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRoutes)
 
 export default app;
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,10 +1,10 @@
-const async=(reqhandler)=>{
-    (req,res,next)=>{
+const asynchandler=(reqhandler)=>{
+   return (req,res,next)=>{
         Promise.resolve(reqhandler(req,res,next)).catch((err)=> next(err))
   }
 }
 
-export default async;
+export  {asynchandler}
 
 
 // const async=(fn)=>async(req,res,next)=>{
